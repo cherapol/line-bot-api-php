e4bd88352eb67e2d4778376a2d0a05169617610e
cherapol <53294374+cherapol@users.noreply.github.com>
2019-07-26 09:54:50 +0700

    Update netpie.php

--- netpie.php
+++ netpie.php
@@ -1,8 +1,8 @@
  <?php
 	function pubMqtt($topic,$msg){
-		$APPID= "LineBotLED/"; //enter your appid
-		$KEY = "OCbJNzksoFmEdXC"; //enter your web key
-		$SECRET = "UwTwehyr7ikZnXcJv7acvwTQq"; //enter your secret
+		$APPID= "pedEGAT"; //enter your appid
+		$KEY = "XiSW3rnqv2rX6GK"; //enter your web key
+		$SECRET = "YkqqactOEZMN4gdSVeYojy8AF"; //enter your secret
 		$Topic = "$topic";
 		if($msg =="on"){
 			$msg = "1";
